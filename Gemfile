00f69ddcecbade6df04f49012c6cb848d38bb6e3
Serlle Rosales <serlle.rosales96@gmail.com>
2025-09-09 12:54:27 -0600

    first configurations

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.0.3"
 
+gem 'concurrent-ruby', '1.3.4'
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.8", ">= 7.0.8.4"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root "weather#new"
+  resources :weather, only: %i[new create]
+  post "/buscar", to: "weather#create", as: "buscar_weather"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
