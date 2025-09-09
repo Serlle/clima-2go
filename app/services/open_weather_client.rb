abe374e8339d7fd5f02b4a5423d83e0e931f1cd5
Serlle Rosales <serlle.rosales96@gmail.com>
2025-09-09 13:56:44 -0600

    feat: Modified the OpenWeatherClient service for path geocoding and retrieving current weather successfully

--- app/services/open_weather_client.rb
+++ app/services/open_weather_client.rb
@@ -4,20 +4,23 @@ require "uri"
 require "json"
 
 class OpenWeatherClient
-  BASE = "https://api.openweathermap.org/data/2.5/weather"
+  BASE = "https://api.openweathermap.org/"
 
   def initialize(api_key)
     @api_key = api_key
   end
 
   # Geocoding for zip(MX)
+  # https://openweathermap.org/api/geocoding-api#direct_zip
   def geocoding_zip(zip, country: "MX")
-    uri = URI.parse("#{BASE}/geo/1.0/zip") #duda
-    uri.query = URI.encode_www_form(zip: "#{zip}", country: "#{country}", api: @api_key)
+    uri = URI.parse("#{BASE}/geo/1.0/zip")
+    uri.query = URI.encode_www_form(zip: "#{zip},#{country}", appid: @api_key)
 
     json_get(uri)
   end
 
+  # 2) Clima actual por lat/lon
+  # https://openweathermap.org/current
   def current_weather(lat:, lon:, lang: "es", units: "metric")
     uri = URI.parse("#{BASE}/data/2.5/weather")
     uri.query = URI.encode_www_form(lat: lat, lon: lon, lang: lang, units: units, appid: @api_key)
@@ -29,7 +32,10 @@ class OpenWeatherClient
 
   def json_get(uri)
     res = Net::HTTP.get_response(uri)
+
+    return nil if res.is_a?(Net::HTTPNotFound) || res.code == "400"
     raise StandardError, "HTTP #{res.code}" unless res.is_a?(Net::HTTPSuccess)
+    
     JSON.parse(res.body)
   end
 end
